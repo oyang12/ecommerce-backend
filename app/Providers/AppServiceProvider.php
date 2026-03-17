71645caa8e959f56b8346c37b21e9fad78a59ca4
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-18 00:23:55 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
         }
     
         // Izinkan Vercel mengakses API Laravel
-        header('Access-Control-Allow-Origin: https://ecommerce-frontend-1ppx.vercel.app');
+        header('Access-Control-Allow-Origin: https://ecommerce-frontend-86o8.vercel.app/');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
     }
