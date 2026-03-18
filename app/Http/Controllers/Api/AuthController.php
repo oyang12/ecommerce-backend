3e6432a4923b2cf9b14702fb4d956dd108112dfb
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-18 17:47:57 +0700

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Api;
+namespace App\Http\Controllers\Api;
 
 // Memastikan alamat induk benar agar tidak Error 500
-use App\Http\Controller; 
+use App\Http\Controllers\Controller; 
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
