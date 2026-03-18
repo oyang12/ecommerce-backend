b7ddad3906fcd7ab3ad7858b7c0fa665b43f982e
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-18 17:33:43 +0700

    Update ProductController.php

--- app/Http/Api/ProductController.php
+++ app/Http/Api/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http/Controllers\Controller;
+use App\Http\Api\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\ProductImage;
