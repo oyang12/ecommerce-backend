52b452e45e5a627cf89a7b2fe884d89cec4e24ee
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-18 15:59:07 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,10 +10,20 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // 1. Membuat Akun Admin
         User::create([
-            'name' => 'Admin MyStore',
-            'email' => 'admin@mystore.com',
-            'password' => Hash::make('password123'), // Ganti password ini nanti!
+            'name'     => 'Admin MyStore',
+            'email'    => 'admin@mystore.com',
+            'password' => Hash::make('password123'), // Jangan lupa ganti nanti ya!
+            'role'     => 'admin',
+        ]);
+
+        // 2. Membuat Akun Pengguna Biasa (Customer)
+        User::create([
+            'name'     => 'Budi Pembeli',
+            'email'    => 'budi@gmail.com',
+            'password' => Hash::make('password123'),
+            'role'     => 'customer',
         ]);
     }
 }
