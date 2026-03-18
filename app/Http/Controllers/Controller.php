d5e3309a29a3e708798350f29d66a2063eb714f1
oyang12 <SURYAAGUSVK7@GMAIL.COM>
2026-03-18 17:47:43 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Api;
+namespace App\Http\Controllers\Api;
 
 // Memastikan AuthController mengenali induknya di App\Http\Controllers\Controller
-use App\Http\Controller;
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
